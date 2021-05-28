c691debfe0099d04cbd9c1d955241569b1ec3033
Fabian Ulrich <fabian.ulrich@stud.uni-bamberg.de>
2021-05-28 13:44:07 +0200

    added decibel recording function to arduinoserial.py, added Arduino_Environment code, added example_data and example_data_decibel for the serial output data and decibel read data

---
+++ Arduino/Arduino-Environment/Arduino_Environment.ino
@@ -0,0 +1,170 @@
+// Initialize DS3231 Time Module
+#include <Wire.h>
+#include "RTClib.h"
+RTC_DS3231 rtc;
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+String timeLabel = "Time";
+
+
+bool century = false;
+bool h12Flag;
+bool pmFlag;
+
+// Initialize Light Sensor:
+int lightInput = A1;
+int lightStatus = 0; // variable for reading the pin status
+String lightLabel = "Photoresistor";
+
+
+//Initialize DHT 22 Humidity Sensor
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+// Set DHT pin:
+#define DHTPIN 2
+
+String humidityLabel = "Humidity";
+String temperatureLabel = "Temperature";
+// Set DHT type, uncomment whatever type you're using!
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+// Initialize DHT sensor for normal 16mhz Arduino:
+DHT dht = DHT(DHTPIN, DHTTYPE);
+
+
+//Initialize CO2 Sensor
+#include <MHZ19.h>
+#include <SoftwareSerial.h>
+
+int MHZ_TX_PIN = 5;  // TX Pin von MH-Z19C Digital pins --> Green
+int MHZ_RX_PIN = 6;  // RX Pin von MH-Z19C Digital pins --> Blue
+String co2Label = "CO2";
+
+// Objekt for the Sensor
+MHZ19 co2Sensor;
+SoftwareSerial co2Serial(MHZ_TX_PIN, MHZ_RX_PIN);
+
+// Soil Moisture Sensor
+const int dry = 595; // value for dry sensor
+const int wet = 239; // value for wet sensor
+String moistureLabe1 = "Soil Moisture";
+
+// Sound Sensor
+const int soundInput = A3;
+int soundValue = 0;
+String soundLabe1 = "Noise";
+
+bool label = true;
+void setup() {
+  // Begin serial communication at a baud rate of 9600:
+  Serial.begin(9600);
+  delay(3000);
+  // Setup DS3231 Time Module
+ if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, lets set the time!");
+ 
+  // Comment out below lines once you set the date & time.
+    // Following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  
+  }
+
+  //Setup Light Sensor
+  pinMode(lightInput, INPUT);
+  // Setup Humidity Sensor
+  dht.begin();
+  //Setup CO2 Sensor
+  co2Serial.begin(9600);
+  co2Sensor.begin(co2Serial);
+  // Automatic calibration
+  co2Sensor.autoCalibration(false);
+
+   
+}
+void loop() {
+    while(label){
+    Serial.print(timeLabel);
+    Serial.print("\t");
+    Serial.print(soundLabe1);
+    Serial.print("\t");
+    Serial.print(lightLabel);
+    Serial.print("\t");
+    Serial.print(humidityLabel);
+    Serial.print("\t");
+    Serial.print(temperatureLabel);
+    Serial.print("\t");
+    Serial.print(co2Label);
+    Serial.print("\t");
+    Serial.print(moistureLabe1);
+    
+    
+
+
+    label = false;
+  }
+
+ // Wait a few seconds between measurements:
+  delay(10000);
+
+ // Read Time
+   DateTime now = rtc.now();
+    Serial.println("\n");
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.print("\t");
+
+
+soundValue = analogRead(soundInput);   
+Serial.print(soundValue);
+Serial.print("\t");
+
+ // Read light status
+ lightStatus = analogRead(lightInput);
+ Serial.print(lightStatus); 
+ Serial.print("\t");
+ 
+  // Read the humidity in %:
+  float humidityValue = dht.readHumidity();
+  // Read the temperature as Celsius:
+  float temperatureValue = dht.readTemperature();
+  
+  // Check if any reads failed and exit early (to try again):
+  if (isnan(humidityValue) || isnan(temperatureValue)){
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print(humidityValue);
+  Serial.print(" % ");
+  Serial.print("\t");
+  Serial.print(temperatureValue);
+  Serial.print(" \xC2\xB0");
+  Serial.print("C");
+  Serial.print("\t");
+
+  //Check CO2 in ppm
+  int co2Content = co2Sensor.getCO2();
+  Serial.print(co2Content);
+  Serial.print("\t");
+
+  // Soil Moisture
+   int sensorVal = analogRead(A0);
+   int percentageHumididy = map(sensorVal, wet, dry, 100, 0); 
+   Serial.print(percentageHumididy);
+   Serial.print("%");
+ 
+}
--- Arduino/Serial-Output/arduinoserial.py
+++ Arduino/Serial-Output/arduinoserial.py
@@ -1,13 +1,45 @@
 import serial
+import audioop
+import pyaudio
+import math
+from datetime import datetime
+import numpy as np
+import pandas as pd
+
+SAMPLING_RATE = 22050
+NUM_SAMPLES = 2048
+
+
+def decibel_recording(decibel_array):
+    pa = pyaudio.PyAudio()
+    stream = pa.open(format=pyaudio.paInt16, channels=2, rate=SAMPLING_RATE,
+                     input=True, frames_per_buffer=NUM_SAMPLES)
+
+    data = stream.read(NUM_SAMPLES)
+    rms = audioop.rms(data, 2)
+    decibel = 20 * math.log10(rms)
+    decibel = str(decibel)
+    now = datetime.now()
+    ts = now.strftime("%H:%M:%S")
+    ts = str(ts)
+    temp_array = np.array([ts, decibel])
+    merged_array = np.vstack((decibel_array, temp_array))
+
+    return merged_array
+
+
+def serial_recording(file_name, serial_port):
+    serial_file = file_name + ".csv"
+    getdata = serial_port.readline()
+    data = getdata.decode('utf-8')
+    print(data)
+    file = open(serial_file, "a")
+    file.write(data + "\n")
+    file.close()
 
 
 def main():
     global sample_number
-
-    arduino_port = "COM5"
-    baud = 9600
-    file_name = input("File name: ")
-    file_name = file_name + ".csv"
     while True:
         try:
             sample_number = int(input("Sample number to collect: "))
@@ -15,22 +47,19 @@ def main():
         except ValueError:
             print("Please input integer only...")
             continue
-    serial_port = serial.Serial(arduino_port, baud)
-
-    print("Connected to Arduino port:" + arduino_port)
-    file = open(file_name, "w")
+    decibel_array = np.array(["Time", "Decibel"])
+    arduino_port = "COM5"
+    baud = 9600
+    file_name = input("File name: ")
     line = 0
-
+    serial_port = serial.Serial(arduino_port, baud)
     while line <= sample_number:
-        getdata = serial_port.readline()
-        data = getdata.decode('utf-8')
-        print(data)
-        file = open(file_name, "a")
-        file.write(data)
+        serial_recording(file_name, serial_port)
+        decibel_array = decibel_recording(decibel_array)
         line = line + 1
 
+    pd.DataFrame(decibel_array).to_csv(file_name + "_decibel.csv", index=False)
     print("Data collection complete!")
-    file.close()
 
 
 if __name__ == '__main__':
